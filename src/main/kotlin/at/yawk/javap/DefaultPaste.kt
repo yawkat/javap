4f12b79eb7b989970837ec6c2d6f0f517c28fb63
yawkat <me@yawk.at>
2016-05-27 12:03:47 +0200

    Some package declarations for the default paste

--- src/main/kotlin/at/yawk/javap/DefaultPaste.kt
+++ src/main/kotlin/at/yawk/javap/DefaultPaste.kt
@@ -8,8 +8,18 @@ import javax.inject.Inject
  * @author yawkat
  */
 class DefaultPaste @Inject constructor(jdkProvider: JdkProvider, processor: Processor) {
-    private val input = ProcessingInput(
-            "public class Main {\n    public Main() {\n        int i = 0;\n        i++;\n    }\n}",
+    private val input = ProcessingInput("""import java.util.*;
+import java.lang.*;
+import java.lang.invoke.*;
+import java.lang.reflect.*;
+import java.io.*;
+
+public class Main {
+    public Main() {
+        int i = 0;
+        i++;
+    }
+}""",
             jdkProvider.defaultJdk.name)
     val defaultPaste = Paste(DEFAULT_PASTE_NAME, "", input, processor.process(input))
 }
