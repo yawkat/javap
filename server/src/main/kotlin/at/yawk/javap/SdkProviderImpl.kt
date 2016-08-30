19cc0e27f381ee5bc5f0cf4a301e3dd792678a57
yawkat <me@yawk.at>
2016-08-30 16:56:29 +0200

    MPL license headers

--- server/src/main/kotlin/at/yawk/javap/SdkProviderImpl.kt
+++ server/src/main/kotlin/at/yawk/javap/SdkProviderImpl.kt
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package at.yawk.javap
 
 import com.google.common.hash.Hashing
