46c78839a8c4af108347c0f32145298f782dd513
Kartik Ohri <kartikohri13@gmail.com>
2020-10-17 10:44:36 +0530

    Specify dependency versions in Versions.kt

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val kotlin = "1.4.10"
+    const val kotlinxSerialization = "1.0.0"
+    const val guice = "4.2.3"
+    const val ztExec = "1.9"
+    const val guava = "28.2-jre"
+    const val flyway = "4.0.1"
+    const val h2 = "1.4.191"
+    const val procyon = "0.5.36"
+    const val testng = "6.9.10"
+    const val mockito = "2.0.2-beta"
+    const val jimfs = "1.1"
+}
