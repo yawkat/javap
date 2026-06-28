f4e8f2bcfe127b0cf58c3d95a7389d9dd2e6cdcf
Jonas Konrad <me@yawk.at>
2026-06-28 11:50:17 +0200

    improve SdkIntegrationTest

--- server/src/test/kotlin/at/yawk/javap/SdkIntegrationTest.kt
+++ server/src/test/kotlin/at/yawk/javap/SdkIntegrationTest.kt
@@ -24,11 +24,15 @@ class SdkIntegrationTest {
 
     @DataProvider
     fun sdks(): Array<Array<Any>> {
-        return Sdks.sdksByName.values.map { arrayOf<Any>(it) }.toTypedArray()
+        return Sdks.sdksByName.values
+                .distinctBy { it.name }
+                .map { arrayOf<Any>(it.name, it) }
+                .toTypedArray()
     }
 
     @Test(dataProvider = "sdks")
-    fun testSdk(sdk: Sdk) {
+    fun testSdk(name: String, sdk: Sdk) {
+        Assert.assertEquals(name, sdk.name)
         val processor = LocalProcessor(sdkProvider, Bubblewrap())
         val testCode = when (sdk.language) {
             SdkLanguage.JAVA -> """
@@ -54,9 +58,9 @@ class SdkIntegrationTest {
             compilerConfiguration = compilerConfiguration + ("source" to 8)
         }
         val output = processor.process(ProcessingInput(testCode, sdk.name, compilerConfiguration))
-        Assert.assertTrue(output.compilerLog.isEmpty(), output.compilerLog)
-        Assert.assertNotNull(output.procyon)
-        Assert.assertFalse(output.procyon!!.contains("Exception: "))
-        Assert.assertNotNull(output.javap)
+        Assert.assertTrue(output.compilerLog.isEmpty(), "${sdk.name}: ${output.compilerLog}")
+        Assert.assertNotNull(output.procyon, sdk.name)
+        Assert.assertFalse(output.procyon!!.contains("Exception: "), sdk.name)
+        Assert.assertNotNull(output.javap, sdk.name)
     }
 }
